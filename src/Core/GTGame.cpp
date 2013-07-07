47758edee16cb985f8c3825bc1b59cdfff0c0c8e
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-07 22:21:57 +0200

    Adapted to newest sses

--- src/Core/GTGame.cpp
+++ src/Core/GTGame.cpp
@@ -66,7 +66,7 @@ namespace gt
 
 	void GTGame::spawnAnts()
 	{
-		for(const auto& ant : manager.getComponents<GTCAnt>()) ant->getEntity().destroy();
+		for(const auto& ant : manager.getEntities("ant")) ant->destroy();
 		for(const auto& o : population.organisms) factory.createAnt(Vec2i{64000, 48000}, *o);
 	}
 
@@ -156,7 +156,7 @@ namespace gt
 	{
 		int winnerCount{0};
 
-		for(const auto& c : manager.getComponents<GTCAnt>()) if(c->isWinner()) ++winnerCount;
+		for(const auto& c : manager.getEntities("ant")) if(c->getFirstComponent<GTCAnt>().isWinner()) ++winnerCount;
 
 		generationTimer = 0;
 
